8322b00f576e33e9c5c2a3f08b0287a49b470469
a.shipulin <sfmrs@mail.ru>
2016-09-30 13:42:57 +0300

    Тест заливки нового файла в проекте

--- src/ibm_java_basis/Card.java
+++ src/ibm_java_basis/Card.java
@@ -3,5 +3,25 @@ package ibm_java_basis;
 /**
  * Created by a.shipulin on 30.09.16.
  */
-public class Card {
+public class Card
+
+{
+    private String name;
+
+    public Card() {
+        name = "unknown";
+    }
+
+    public Card(String name1) {
+        name = name1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return getName();
+    }
 }
+
