df60f23897dbd3e410a0184c2933656447c50b23
a.shipulin <sfmrs@mail.ru>
2016-09-30 11:08:39 +0300

    Тест заливки нового файла в проекте

---
+++ src/learn_collections/TestGitHubPush.java
@@ -0,0 +1,10 @@
+package learn_collections;
+
+/**
+ * Created by a.shipulin on 30.09.16.
+ */
+public class TestGitHubPush {
+    public static void main(String[] args) {
+        System.out.println("TestGitHubPush");
+    }
+}
